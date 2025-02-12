5cf60387056ec772361de6c181012895de9332ab
El Bouchtili Imaddine <93282364+Hasenko@users.noreply.github.com>
2025-02-12 13:20:37 +0100

    resolving ROX and Rookie

---
+++ CyberCruisade2/Crypto/resolve.py
@@ -0,0 +1,18 @@
+"""
+flag.txt : 68f26cf17bc979d9428e73d85fe359d668a452f2438c64a85f93569a528c5fe4309055f554c331a04beb539c3f9f30bd5fee30bb3ff633f433fb34f937a955ef72e347c5
+
+commencer par convertir flag.txt en ascii. De la on remarque que 1 charactère sur 2 est fait partie du flag.
+Donc on prend sépare flag.txt en 2 (partie correct et partie incorrect).
+
+On fait un XNOR de la bonne partie sur la mauvaise et on obtien les bon caractères.
+Il n'y a plus qu'a superposé.
+
+686c7b7942735f59685243645f56525f305554314b533f305f303f33333437557247
+XNOR
+f2f1c9d98ed8e3d6a4f28ca8939a8ce490f5c3a0eb9c9fbdeebbf6f4fbf9a9efe3c5
+
+hl{yBs_YhRCd_VR_0UT1KS?0_0?3347UrG
+ebM_3TCp3_0333!D__hn_0_rNt6872aEn}
+
+helb{My_B3sT_CYph3R_C0d3_3V3R!_D0_U_Th1nK_S0?_0r_N0t?63837427aUErnG}
+"""
